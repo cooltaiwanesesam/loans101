b1f3db3c43f7e27b7ddcdd464bc722404b076026
cooltaiwanesesam <cooltaiwaneseam@gmail.com>
2017-09-20 21:43:20 +1000

    Update profile on homepage

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-$(function(){
-    var current = location.pathname;
-    $('#profile ul li a').each(function(){
-        var $this = $(this);
-        // if the current path is like this link, make it active
-        if($this.attr('href').indexOf(current) !== -1){
-            $this.addClass('active');
-        }
-    })
-})
-
 (function($){
     // Caption
     $('.article-entry').each(function(i){

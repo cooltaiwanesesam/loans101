86b594f8532852cd9652fffc628725d75f3140a0
cooltaiwanesesam <cooltaiwaneseam@gmail.com>
2017-10-02 16:35:27 +1100

    update FAB

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 (function($){
-    // Answer toggle
-    $(document).ready(function() {
-      $('.answer .button').click (function() {
-        $('.summary').slideToggle();
-      })  
-    });
+  //FAB scroll
+  //https://jsfiddle.net/d00h1zmn/4/
+  var senseSpeed = 5;
+  var previousScroll = 0;
+  $(window).scroll(function(event){
+     var scroller = $(this).scrollTop();
+     if (scroller-senseSpeed > previousScroll){
+        $("#fab").filter(':not(:animated)').fadeOut();
+     } else if (scroller+senseSpeed < previousScroll) {
+        $("#fab").filter(':not(:animated)').fadeIn();
+     }
+     previousScroll = scroller;
+  });
+
+  // Answer toggle
+  $(document).ready(function() {
+    $('.answer .button').click (function() {
+      $('.summary').slideToggle();
+    })  
+  });
 })(jQuery);
 
 
